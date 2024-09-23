91e154dfc05b9146ee70fa6324de85df89ffee9f
Juan Cruz Od <johnmanito3@gmail.com>
2024-09-23 01:45:34 -0400

    UpConferencias

--- src/Cursos.tsx
+++ src/Cursos.tsx
@@ -2,7 +2,11 @@ import  "./index.css"
 // import React from 'react'
 import ReactPlayer from 'react-player'
 import NavCursos from './NavCursos'
-import Pink from "/A.W.Pink-Los_Atributos_de_Dios.pdf";
+import Numericas from "/Conferencia _Series_Numericas.pdf";
+import Potencia from "/Conferencia_Series_de_Potencia.pdf";
+import Edo from "/Conferencia_EDO.pdf";
+import EdoN from "/Conferencia_EDO_N.pdf";
+import Sledo from "/Conferencia_SLEDO.pdf";
 
 export default function Cursos () {
 
@@ -51,19 +55,19 @@ return (
 
 
 
-<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
+<button type="button" className="btn margin-top-bottom btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Indicaciones
 </button>
 
 <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
-        <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 className="modal-title fs-5" id="exampleModalLabel">Que hago?</h1>
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
-        ...
+        En esta seccion podras encontrar las conferencias tocante al curso de Matematica III de la Universidad de Ciego de Avila
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -77,79 +81,139 @@ return (
 
 
 
-<p>
+{/* <p>
   <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
     Toggle width collapse
   </button>
-</p>
+</p> */}
 
 
 
 
 
 
-<h2>Series Numericas aja</h2>
 
-<div className="container-fluid">
+{/* <div className="container-fluid">
   <ReactPlayer 
   url='https://www.youtube.com/watch?v=q3PKNySW6LQ&list=PL9SnRnlzoyX0RE6_wcrTKaWj8cmQb3uO6'
-    width='100%'
-    // height='50%'
-    controls
-    loop
-    volume={.5}
-    />
-      
-</div>
+  width='100%'
+  // height='50%'
+  controls
+  loop
+  volume={.5}
+  />
+  
+  </div> */}
 
-<h3>Pink Los Atributos de Dios</h3>
+
+<h1>Conferencias</h1>
+
+
+  <h2>Series Numericas</h2>
 
 <div>
     <div className='btn btn-primary'>
-      <a className="fs-6 text-light"  href={Pink} target="_blank" rel="noopener"> 
+      <a className="fs-6 text-light"  href={Numericas} target="_blank" rel="noopener"> 
       Ver PDF
       </a>
     </div>
 </div>
 
 
-<div>
+<div className="margin-top-bottom">
     <div className='btn btn-primary'>
-      <a className="fs-6 text-light"  href={Pink} target="_blank" rel="noopener noreferrer" download="Pink Los Atributos de Dios.pdf"> 
+      <a className="fs-6 text-light"  href={Numericas} target="_blank" rel="noopener noreferrer" download="Conferencia Series Numericas.pdf"> 
       Descargar PDF
       </a>
     </div>
 </div>
 
 
-<h2>Series de Potencias</h2>
 
 
-<div className="container-fluid">
-  <ReactPlayer 
-  url='https://www.youtube.com/watch?v=_w4sPyiNdBY'
-    width='100%'
-    // height='100%'
-    controls
-    loop
-    volume={.5}
-    />
-      
+
+
+  <h2>Series de Potencias</h2>
+
+<div>
+    <div className='btn btn-primary'>
+      <a className="fs-6 text-light"  href={Potencia} target="_blank" rel="noopener"> 
+      Ver PDF
+      </a>
+    </div>
 </div>
 
 
-<h2></h2>
 
-<div className="container-fluid">
-  <ReactPlayer 
-  url='https://www.youtube.com/watch?v=GWS3PCBFVgI'
-    width='100%'
-    // height='100%'
-    controls
-    loop
-    volume={.5}
-    />
-      
+<div className="margin-top-bottom">
+    <div className='btn btn-primary'>
+      <a className="fs-6 text-light"  href={Potencia} target="_blank" rel="noopener noreferrer" download="Conferencia Series de Potencia.pdf"> 
+      Descargar PDF
+      </a>
+    </div>
+</div>
+
+
+
+
+<h2>Ecuaciones diferenciales de Primer Orden</h2>
+
+<div>
+    <div className='btn btn-primary'>
+      <a className="fs-6 text-light"  href={Edo} target="_blank" rel="noopener"> 
+      Ver PDF
+      </a>
+    </div>
+</div>
+
+
+
+<div className="margin-top-bottom">
+    <div className='btn btn-primary'>
+      <a className="fs-6 text-light"  href={Edo} target="_blank" rel="noopener noreferrer" download="Conferencia EDO de primer orden.pdf"> 
+      Descargar PDF
+      </a>
+    </div>
+</div>
+
+
+<h2>Ecuaciones diferenciales de Orden Superior</h2>
+
+<div>
+    <div className='btn btn-primary'>
+      <a className="fs-6 text-light"  href={EdoN} target="_blank" rel="noopener"> 
+      Ver PDF
+      </a>
+    </div>
+</div>
+
+
+<div className="margin-top-bottom">
+    <div className='btn btn-primary'>
+      <a className="fs-6 text-light"  href={EdoN} target="_blank" rel="noopener noreferrer" download="Conferencia EDO de orden n-esimo.pdf"> 
+      Descargar PDF
+      </a>
+    </div>
+</div>
+
+
+<h2>Sistema de Ecuaciones Diferenciales Lineales</h2>
+
+<div>
+    <div className='btn btn-primary'>
+      <a className="fs-6 text-light"  href={Sledo} target="_blank" rel="noopener"> 
+      Ver PDF
+      </a>
+    </div>
+</div>
+
+
+<div className="margin-top-bottom">
+    <div className='btn btn-primary'>
+      <a className="fs-6 text-light"  href={Sledo} target="_blank" rel="noopener noreferrer" download="Conferencia SLEDO.pdf"> 
+      Descargar PDF
+      </a>
+    </div>
 </div>
 
 
